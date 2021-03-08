35b9d937fa8a21b3399807626cf0578bc77b7e24
MatiasDevop <solomatias321@gmail.com>
2021-03-08 19:16:54 +0100

    adding dbset and mapping for entities

--- src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
+++ src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookStore.Infrastructure.Context
 {
-    class BookStoreDbContext
+    public class BookStoreDbContext : DbContext
     {
+        public BookStoreDbContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
