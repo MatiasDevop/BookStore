8de8b2bed7418838abd35a283fba99f7d8447620
MatiasDevop <solomatias321@gmail.com>
2021-03-09 12:02:50 +0100

    adding mapping and setting up EF,\ dbcontext and migrations

--- src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
+++ src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BookStore.Infrastructure.Context
@@ -13,5 +14,20 @@ namespace BookStore.Infrastructure.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                    .SelectMany(e => e.GetProperties()
+                            .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(150)");
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
