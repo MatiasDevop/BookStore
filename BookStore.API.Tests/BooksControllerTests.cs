6c628c92a625d4c3b3d03e46bd1c57a2166242d5
MatiasDevop <solomatias321@gmail.com>
2021-03-25 13:09:06 +0100

    implementing testing on domain and infra layer

--- BookStore.API.Tests/BooksControllerTests.cs
+++ BookStore.API.Tests/BooksControllerTests.cs
@@ -56,7 +56,7 @@ namespace BookStore.API.Tests
         public async void GetAll_ShouldCallGetAllFromService_OnlyOnce()
         {
             var books = CreateBookList();
-            var dtoExpected = MapModelToBookResultListDto();
+            var dtoExpected = MapModelToBookResultListDto(books);
 
             _bookServiceMock.Setup(c => c.GetAll()).ReturnsAsync(books);
             _mapperMock.Setup(m => m.Map<IEnumerable<BookResultDto>>(It.IsAny<List<Book>>())).Returns(dtoExpected);
@@ -70,7 +70,7 @@ namespace BookStore.API.Tests
         public async void GetById_ShouldReturnOk_WhenBookExist()
         {
             var book = CreateBook();
-            var dtoExpected = new MapModelToBookResultDto(book);
+            var dtoExpected = MapModelToBookResultDto(book); 
 
             _bookServiceMock.Setup(c => c.GetById(2)).ReturnsAsync(book);
             _mapperMock.Setup(m => m.Map<BookResultDto>(It.IsAny<Book>())).Returns(dtoExpected);
@@ -80,6 +80,161 @@ namespace BookStore.API.Tests
             Assert.IsType<OkObjectResult>(result);
         }
 
+        [Fact]
+        public async void GetById_ShouldReturnNotFound_WhenBookDoesNotExist()
+        {
+            _bookServiceMock.Setup(c => c.GetById(2)).ReturnsAsync((Book)null);
 
+            var result = await _booksController.GetById(2);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async void GetById_ShouldCallGetByIdFromService_OnlyOnce()
+        {
+            var book = CreateBook();
+            var dtoExpected = MapModelToBookResultDto(book);
+
+            _bookServiceMock.Setup(c => c.GetById(2)).ReturnsAsync(book);
+            _mapperMock.Setup(m => m.Map<BookResultDto>(It.IsAny<Book>())).Returns(dtoExpected);
+
+            await _booksController.GetById(2);
+
+            _bookServiceMock.Verify(mock => mock.GetById(2), Times.Once);
+        }
+
+        [Fact]
+        public async void GetBooksByCategory_ShouldReturnOk_WhenBookWithSearchedCategoryExist()
+        {
+            var bookList = CreateBookList();
+            var book = CreateBook();
+            var dtoExpected = MapModelToBookResultListDto(bookList);
+
+            _bookServiceMock.Setup(c => c.GetBooksByCategory(book.CategoryId)).ReturnsAsync(bookList);
+            _mapperMock.Setup(m => m.Map<IEnumerable<BookResultDto>>(It.IsAny<IEnumerable<Book>>())).Returns(dtoExpected);
+
+            var result = await _booksController.GetBooksByCategory(book.CategoryId);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async void GetBooksByCategory_ShouldReturnNotFound_WhenBookWithSearchedCategoryDoesNotExist()
+        {
+            var book = CreateBook();
+            var dtoExpected = MapModelToBookResultDto(book);
+
+            _bookServiceMock.Setup(c => c.GetBooksByCategory(book.CategoryId)).ReturnsAsync(new List<Book>());
+            _mapperMock.Setup(m => m.Map<BookResultDto>(It.IsAny<Book>())).Returns(dtoExpected);
+
+            var result = await _booksController.GetBooksByCategory(book.CategoryId);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async void GetBooksByCategory_ShouldCallGetBooksByCategoryFromService_OnlyOnce()
+        {
+            var bookList = CreateBookList();
+            var book = CreateBook();
+            var dtoExpected = MapModelToBookResultListDto(bookList);
+
+            _bookServiceMock.Setup(c => c.GetBooksByCategory(book.CategoryId)).ReturnsAsync(bookList);
+            _mapperMock.Setup(m => m.Map<IEnumerable<BookResultDto>>(It.IsAny<IEnumerable<Book>>())).Returns(dtoExpected);
+
+            await _booksController.GetBooksByCategory(book.CategoryId);
+
+            _bookServiceMock.Verify(mock => mock.GetBooksByCategory(book.CategoryId), Times.Once);
+        }
+
+        private Book CreateBook()
+        {
+            return new Book()
+            {
+                Id = 2,
+                Name = "Book Test",
+                Author = "Author Test",
+                Description = "Description Test",
+                Value = 10,
+                CategoryId = 1,
+                PublishDate = DateTime.MinValue.AddYears(40),
+                Category = new Category()
+                {
+                    Id = 1,
+                    Name = "Category Test"
+                }
+            };
+        }
+
+        private BookResultDto MapModelToBookResultDto(Book book)
+        {
+            var bookDto = new BookResultDto()
+            {
+                Id = book.Id,
+                Name = book.Name,
+                Author = book.Author,
+                Description = book.Description,
+                PublishDate = book.PublishDate,
+                Value = book.Value,
+                CategoryId = book.CategoryId
+            };
+            return bookDto;
+        }
+
+        private List<Book> CreateBookList()
+        {
+            return new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Name = "Book Test 1",
+                    Author = "Author Test 1",
+                    Description = "Description Test 1",
+                    Value = 10,
+                    CategoryId = 1
+                },
+                new Book()
+                {
+                    Id = 1,
+                    Name = "Book Test 2",
+                    Author = "Author Test 2",
+                    Description = "Description Test 2",
+                    Value = 20,
+                    CategoryId = 1
+                },
+                new Book()
+                {
+                    Id = 1,
+                    Name = "Book Test 3",
+                    Author = "Author Test 3",
+                    Description = "Description Test 3",
+                    Value = 30,
+                    CategoryId = 2
+                }
+            };
+        }
+
+        private List<BookResultDto> MapModelToBookResultListDto(List<Book> books)
+        {
+            var listBooks = new List<BookResultDto>();
+
+            foreach (var item in books)
+            {
+                var book = new BookResultDto()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Author = item.Author,
+                    Description = item.Description,
+                    PublishDate = item.PublishDate,
+                    Value = item.Value,
+                    CategoryId = item.CategoryId
+                };
+                listBooks.Add(book);
+            }
+            return listBooks;
+        }
     }
 }
--- BookStore.Infrastructure.Tests/BookRepositoryTests.cs
+++ BookStore.Infrastructure.Tests/BookRepositoryTests.cs
@@ -61,12 +61,118 @@ namespace BookStore.Infrastructure.Tests
             {
                 var bookRepository = new BookRepository(context);
 
-                var expectedBooks = new BookRepository(context);
+                var expectedBooks = CreateBookList();
                 var bookList = await bookRepository.GetAll();
 
+                Assert.Equal(3, bookList.Count);
+                Assert.Equal(expectedBooks[0].Id, bookList[0].Id);
+                Assert.Equal(expectedBooks[0].Name, bookList[0].Name);
+                Assert.Equal(expectedBooks[0].Description, bookList[0].Description);
+                Assert.Equal(expectedBooks[0].CategoryId, bookList[0].CategoryId);
+                Assert.Equal(expectedBooks[0].PublishDate, bookList[0].PublishDate);
+                Assert.Equal(expectedBooks[0].Value, bookList[0].Value);
+                Assert.Equal(expectedBooks[0].Category.Id, bookList[0].Category.Id);
+                Assert.Equal(expectedBooks[0].Category.Name, bookList[0].Category.Name);
+                Assert.Equal(expectedBooks[1].Id, bookList[1].Id);
+                Assert.Equal(expectedBooks[1].Name, bookList[1].Name);
+                Assert.Equal(expectedBooks[1].Description, bookList[1].Description);
+                Assert.Equal(expectedBooks[1].CategoryId, bookList[1].CategoryId);
+                Assert.Equal(expectedBooks[1].PublishDate, bookList[1].PublishDate);
+                Assert.Equal(expectedBooks[1].Value, bookList[1].Value);
+                Assert.Equal(expectedBooks[1].Category.Id, bookList[1].Category.Id);
+                Assert.Equal(expectedBooks[1].Category.Name, bookList[1].Category.Name);
+                Assert.Equal(expectedBooks[2].Id, bookList[2].Id);
+                Assert.Equal(expectedBooks[2].Name, bookList[2].Name);
+                Assert.Equal(expectedBooks[2].Description, bookList[2].Description);
+                Assert.Equal(expectedBooks[2].CategoryId, bookList[2].CategoryId);
+                Assert.Equal(expectedBooks[2].PublishDate, bookList[2].PublishDate);
+                Assert.Equal(expectedBooks[2].Value, bookList[2].Value);
+                Assert.Equal(expectedBooks[2].Category.Id, bookList[2].Category.Id);
+                Assert.Equal(expectedBooks[2].Category.Name, bookList[2].Category.Name);
 
 
             }
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnBookWithSearChedId_WhenBookWithSearchedIdExist()
+        {
+            await using (var context = new BookStoreDbContext(_options))
+            {
+                var bookRepository = new BookRepository(context);
+
+                var book = await bookRepository.GetById(2);
+
+                Assert.NotNull(book);
+                Assert.IsType<Book>(book);
+            }
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnNull_WhenBookWithSearchedIdDoesNotExist()
+        {
+            await BookStoreHelperTests.CleanDataBase(_options);
+
+            await using (var context = new BookStoreDbContext(_options))
+            {
+                var bookRepository = new BookRepository(context);
+                var book = await bookRepository.GetById(1);
+
+                Assert.Null(book);
+
+            }
+        }
+
+        private List<Book> CreateBookList()
+        {
+            return new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Name = "Book Test 1",
+                    Author = "Author Test 1",
+                    Description = "Description Test 1",
+                    Value = 10,
+                    CategoryId = 1,
+                    PublishDate = new DateTime(2020, 1, 1, 0, 0, 0, 0),
+                    Category = new Category()
+                    {
+                        Id = 1,
+                        Name = "Category Test 1"
+                    }
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Name = "Book Test 2",
+                    Author = "Author Test 2",
+                    Description = "Description Test 2",
+                    Value = 20,
+                    CategoryId = 1,
+                    PublishDate = new DateTime(2020, 2, 2, 0, 0, 0, 0),
+                    Category = new Category()
+                    {
+                        Id = 1,
+                        Name = "Category Test 1"
+                    }
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Name = "Book Test 3",
+                    Author = "Author Test 3",
+                    Description = "Description Test 3",
+                    Value = 30,
+                    CategoryId = 3,
+                    PublishDate = new DateTime(2020, 3, 3, 0, 0, 0, 0),
+                    Category = new Category()
+                    {
+                        Id = 3,
+                        Name = "Category Test 3"
+                    }
+                }
+            };
         }
     }
 }
